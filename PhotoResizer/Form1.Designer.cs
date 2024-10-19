08b1df8f44eae73729f7257a68240d3196a52d6a
Adnan Rahmanpoor <adnan.rp359@gmail.com>
2024-10-19 15:36:47 +0400

    Add project files.

---
+++ PhotoResizer/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace PhotoResizer
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnSave;
+
+        private void InitializeComponent()
+        {
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+
+            // btnUpload
+
+            this.btnUpload.Location = new System.Drawing.Point(12, 12);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(100, 23);
+            this.btnUpload.TabIndex = 0;
+            this.btnUpload.Text = "Upload Image";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+
+            // pictureBox
+            this.pictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox.Location = new System.Drawing.Point(12, 50);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+
+            //btnSave
+
+            this.btnSave.Location = new System.Drawing.Point(12, 470);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save Image";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+
+            // Form1
+
+            this.ClientSize = new System.Drawing.Size(424, 511);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnUpload);
+            this.Name = "Form1";
+            this.Text = "PhotoResizer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ PhotoResizer/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PhotoResizer
+{
+    public partial class Form1: Form
+    {
+        private Image originalImage; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;",
+                Title = "Select an Image"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Load the image
+                originalImage = Image.FromFile(openFileDialog.FileName);
+                pictureBox.Image = originalImage;
+            }
+        }
+
+        // Button to resize and save the image
+        private void btnSave_Click(object sender, EventArgs e) 
+        {
+            if (originalImage != null)
+            {
+                // Resize
+                Image resizedImage = ResizeImage(originalImage, 1000, 1000);
+
+                // Save 
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "JPEG Image|*.jpg;*.jpeg;|PNG Image|*.png;",
+                    Title = "Save Resized Image"
+                };
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Save to the location
+                    resizedImage.Save(saveFileDialog.FileName);
+                    MessageBox.Show("Image save successfully!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please upload an image first.");
+            }
+        }
+
+        // Function to resize image
+        private Image ResizeImage(Image image, int width, int height)
+        {
+            Bitmap resizedBitmap = new Bitmap(width, height); // Create blank bitmap
+            using (Graphics g = Graphics.FromImage(resizedBitmap))
+            {
+                g.DrawImage(image, 0, 0, width, height);
+            }
+            return resizedBitmap;
+        }
+    }
+}
